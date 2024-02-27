2566d0be00e378a57b1df055d0018bcf3b739a93
kou12345 <hi.0412.ko.2002@gmail.com>
2024-02-27 18:45:12 +0900

    ユーザー検索のユースケースを追加

---
+++ api/internal/usecase/find_by_id.go
@@ -0,0 +1,45 @@
+package usecase
+
+import (
+	"context"
+	"errors"
+
+	"github.com/kou12345/go-restapi-example/api/internal/domain/administrator"
+	"github.com/kou12345/go-restapi-example/api/internal/domain/user"
+)
+
+type FindUserByIdUsecase struct {
+	uds user.UserDomainService
+	ads administrator.AdminDomainService
+}
+
+func NewFindUserByIdUsecase(uds user.UserDomainService, ads administrator.AdminDomainService) *FindUserByIdUsecase {
+	return &FindUserByIdUsecase{uds: uds, ads: ads}
+}
+
+// ユーザーIDでユーザーを検索
+func (us *FindUserByIdUsecase) Run(ctx context.Context, id string) (*user.User, error) {
+	value, ok := ctx.Value("user_id").(string)
+	if !ok {
+		return nil, user.ErrInvalidUserID
+	}
+
+	adminUser, err := us.ads.FindUser(ctx, value)
+	if err != nil {
+		return nil, err
+	}
+
+	if adminUser.GetAdmin() == 1 || id == value {
+		user, err := us.uds.FindUserById(ctx, id)
+		if err != nil {
+			return nil, err
+		}
+		return user, nil
+	}
+
+	if adminUser.GetAdmin() != 1 {
+		return nil, administrator.ErrInvalidAdmin
+	}
+
+	return nil, errors.New("unexpected error occurred")
+}
